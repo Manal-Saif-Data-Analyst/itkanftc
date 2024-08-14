3c0ee52a74f63c87dbad8db5c72e53fabc3effbb
Aaliya-code <aalicha72@gmail.com>
2024-08-14 15:14:51 -0500

    Create times table.java

---
+++ times table.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner console = new Scanner(System.in);
+
+        System.out.println("Enter a number: ");
+        int n = console.nextInt();
+        
+        for(int i=1; i<=12; i++){
+            System.out.println(n + "x" + i + "=" + n*i);
+        }
+            
+        
+
+
+      
+        }
+    
+    
+        }
