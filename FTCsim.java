9de212c97b0471bad7765d17ff28bf113dae330f
Aaliya-code <aalicha72@gmail.com>
2024-09-14 00:54:31 -0500

    Create FTCsim.java

---
+++ FTCsim.java
@@ -0,0 +1,36 @@
+public class MyFIRSTJavaOpMode extends LinearOpMode {
+    DcMotor motorLeft;
+    DcMotor motorRight;
+    DcMotor frontLeft;
+    DcMotor frontRight;
+    ColorSensor color1;
+    DistanceSensor distance1;
+    BNO055IMU imu;
+
+@Override
+    public void runOpMode() {
+      motorLeft = hardwareMap.get(DcMotor.class, "motorLeft");
+      motorRight = hardwareMap.get(DcMotor.class, "motorRight");
+      frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+      frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+      color1 = hardwareMap.get(ColorSensor.class, "color1");
+      distance1 = hardwareMap.get(DistanceSensor.class, "distance1");
+      imu = hardwareMap.get(BNO055IMU.class, "imu");
+      // Put initialization blocks here
+      waitForStart();
+      // Put run blocks here
+      motorLeft.setDirection(DcMotor.Direction.REVERSE);
+      motorLeft.setPower(1);
+      motorRight.setPower(-1);
+      sleep(450);
+      motorLeft.setPower(0);
+      motorRight.setPower(0);
+      motorLeft.setPower(0.2);
+      motorRight.setPower(1);
+      
+      while (opModeIsActive()) {
+        // Put loop blocks here
+      }
+    }
+    
+}
